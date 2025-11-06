ebadb0565f2cc8ec0ca774136614bcb8f22ceb96
Brandon Allshouse <covertlaptop@gmail.com>
2025-11-06 02:32:06 -0500

    fixed previous rebasing issue, make sure to pull changes before you try to overwrite with loca, this creates divergent branches

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -41,7 +41,6 @@ class AuthHandler {
         try {
             // Send login request to backend server
             const response = await fetch('http://localhost:8081/login', {
-                
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
