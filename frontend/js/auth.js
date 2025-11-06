6ef208de5e75b29050ededd467c7dfe208d79b13
Tyler Slack <tjs1029@sru.edu>
2025-11-06 01:02:45 -0500

    In auth.js the backend server port was 8081

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -40,7 +40,7 @@ class AuthHandler {
 
         try {
             // Send login request to backend server
-            const response = await fetch('http://localhost:8081/login', {
+            const response = await fetch('http://localhost:8080/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
